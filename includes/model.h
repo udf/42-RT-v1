5c5af4391b401beb9395014159eefe195e583f62
udf <tabhooked@gmail.com>
2018-09-06 16:23:45 +0200

    Generate structures are compile time
    I am so sorry

--- includes/model.h
+++ includes/model.h
@@ -6,7 +6,7 @@
 /*   By: mhoosen <mhoosen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/29 14:56:43 by mhoosen           #+#    #+#             */
-/*   Updated: 2018/09/06 16:22:49 by mhoosen          ###   ########.fr       */
+/*   Updated: 2018/09/06 16:06:59 by mhoosen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,27 @@ union u_object;
 typedef int			(*t_inter_test)(union u_object *o, t_ray ray, float *t);
 typedef t_p3d		(*t_normal_at)(union u_object *o, t_p3d p);
 
+/*
+** Macros for generating structs
+*/
+# define GEN_SPHERE GEN_P3D(pos) GEN_FLOAT(radius)
+# define GEN_PLANE GEN_P3D(norm) GEN_P3D(pos)
+# define GEN_CONE //TODO
+# define GEN_CYLINDER //TODO
+# define GEN_LIGHT GEN_P3D(pos)
+
+/*
+** Wrapper that makes all structs
+*/
+# define GEN_STRUCTS1 GEN_STRUCT(sphere, GEN_SPHERE)
+# define GEN_STRUCTS2 GEN_STRUCT(plane, GEN_PLANE) GEN_STRUCT(cone, GEN_CONE)
+# define GEN_STRUCTS3 GEN_STRUCT(light, GEN_LIGHT)
+# define GEN_STRUCTS4 GEN_STRUCT(cylinder, GEN_CYLINDER)
+# define GEN_STRUCTS GEN_STRUCTS1 GEN_STRUCTS2 GEN_STRUCTS3 GEN_STRUCTS4
+
+/*
+** Enumeration of all object types
+*/
 typedef enum		e_object
 {
 	SPHERE,
@@ -38,6 +59,9 @@ typedef enum		e_object
 	DUMMY
 }					t_e_object;
 
+/*
+** Every object contains a generic struct in the g attribute
+*/
 typedef struct		s_generic
 {
 	t_e_object		type;
@@ -46,47 +70,34 @@ typedef struct		s_generic
 	t_normal_at		normal_at;
 }					t_generic;
 
-typedef struct		s_sphere
-{
-	t_generic		g;
-	t_p3d			pos;
-	float			radius;
-}					t_sphere;
+/*
+** Temporarily define how to make the structures as well as their components
+*/
+# define GEN_P3D(name) t_p3d name;
+# define GEN_FLOAT(name) float name;
+# define GEN_STRUCT(name, body) typedef struct {t_generic g; body} t_##name;
 
-typedef struct		s_cone
-{
-	t_generic		g;
-	// TODO: actual props
-}					t_cone;
+/*
+** Actually make structs
+*/
+GEN_STRUCTS;
 
-typedef struct		s_cylinder
-{
-	t_generic		g;
-	// TODO: actual props
-}					t_cylinder;
+/*
+** Undefine the temporary macros
+*/
+# undef GEN_STRUCT
+# undef GEN_FLOAT
+# undef GEN_P3D
 
-typedef struct		s_plane
-{
-	t_generic		g;
-	t_p3d			norm;
-	t_p3d			pos;
-}					t_plane;
+# define GEN_STRUCT_STUB(t_name, name) t_name name;
+# define GEN_STRUCT(name, body) GEN_STRUCT_STUB(t_##name, name)
+# define GEN_U typedef union u_object {t_generic g; GEN_STRUCTS} t_object;
 
-typedef struct		s_light
-{
-	t_generic		g;
-	t_p3d			pos;
-}					t_light;
+GEN_U;
 
-typedef union		u_object
-{
-	t_generic		g;
-	t_sphere		sphere;
-	t_cone			cone;
-	t_cylinder		cylinder;
-	t_plane			plane;
-	t_light			light;
-}					t_object;
+# undef GEN_U
+# undef GEN_STRUCT
+# undef GEN_STRUCT_STUB
 
 typedef int			(*t_objloader)(char *line, t_object *object);
 
